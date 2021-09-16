63a616ecff2ae0174f69d52f437aceb454b545bd
Rodrigo Galba <rodrigogalba@gmail.com>
2021-09-16 01:19:23 -0300

    simple which(1) unix program

--- app.go
+++ app.go
@@ -1,11 +1,35 @@
 package main
 
-import "fmt"
-
-var i int
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-	fmt.Println("my-cli --help")
-	i = 1
-	fmt.Println(i)
+	args := os.Args
+	if len(args) == 1 {
+		fmt.Println("Please provide an argument")
+		return
+	}
+
+	file := args[1]
+	path := os.Getenv("PATH")
+	pathSplit := filepath.SplitList(path)
+	for _, directory := range pathSplit {
+		fullPath := filepath.Join(directory, file)
+		fileInfo, err := os.Stat(fullPath)
+
+		if err != nil {
+			continue
+		}
+
+		mode := fileInfo.Mode()
+		if mode.IsRegular() {
+			if mode&011 != 0 {
+				fmt.Println(fullPath)
+				return
+			}
+		}
+	}
 }
